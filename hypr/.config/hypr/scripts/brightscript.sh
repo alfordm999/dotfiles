805cc0e7954b33fd693c5de2252bca840f0ea29d
Miles <alfordm999@gmail.com>
2024-12-19 19:34:21 -0500

    first

---
+++ hypr/.config/hypr/scripts/brightscript.sh
@@ -0,0 +1,3 @@
+ddcutil setvcp 10 10 -d 1  
+ddcutil setvcp 10 10 -d 2   
+ddcutil setvcp 10 10 -d 3   
---
+++ hypr/.config/hypr/scripts/brightscriptup.sh
@@ -0,0 +1,3 @@
+ddcutil setvcp 10 100 -d 1  
+ddcutil setvcp 10 100 -d 2   
+ddcutil setvcp 10 100 -d 3   
