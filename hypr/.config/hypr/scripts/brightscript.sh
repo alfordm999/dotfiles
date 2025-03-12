982a76b089fa9129b24611aacf5e045a37eb2e41
alfordm999 <alfordm999@gmail.com>
2025-03-11 20:47:21 -0400

    -

--- hypr/.config/hypr/scripts/brightscript.sh
+++
@@ -1,3 +0,0 @@
-ddcutil setvcp 10 10 -d 1  
-ddcutil setvcp 10 10 -d 2   
-ddcutil setvcp 10 10 -d 3   
--- hypr/.config/hypr/scripts/brightscriptup.sh
+++
@@ -1,3 +0,0 @@
-ddcutil setvcp 10 100 -d 1  
-ddcutil setvcp 10 100 -d 2   
-ddcutil setvcp 10 100 -d 3   
