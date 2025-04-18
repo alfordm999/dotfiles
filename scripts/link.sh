a328855fe6e206b396bae5cffe90f3ce3b2f15e0
alfordm999 <108696717+alfordm999@users.noreply.github.com>
2025-04-18 09:03:34 -0400

    ln script

--- scripts/link.sh
+++ scripts/link.sh
@@ -3,3 +3,16 @@
  mv ~/.zplug ~/.data/.zplug; ln -s ~/.data/.zplug ~/.zplug
  mv ~/.pki ~/.data/.pki; ln -s ~/.data/.pki ~/.pki
  mv ~/.mozilla ~/.data/.mozilla; ln -s ~/.data/.mozilla ~/.mozilla
+ mv ~/.steam ~/.data/.steam; ln -s ~/.data/.steam ~/.steam
+ mv ~/.nv ~/.data/.nv; ln -s ~/.data/.nv ~/.nv
+ mv ~/.vscode-oss ~/.data/.vscode-oss; ln -s ~/.data/.vscode-oss ~/.vscode-oss
+ mv ~/.mono ~/.data/.mono; ln -s ~/.data/.mono ~/.mono
+
+ mv ~/.steampath ~/.data/.steampath; ln -s ~/.data/.steampath ~/.steampath
+ mv ~/.steampid ~/.data/.steampid; ln -s ~/.data/.steampid ~/.steampid 
+ mv ~/.pulse-cookie ~/.data/.pulse-cookie; ln -s ~/.data/.pulse-cookie ~/.pulse-cookie
+ mv ~/.gtkrc-2.0 ~/.data/.gtkrc-2.0; ln -s ~/.data/.gtkrc-2.0 ~/.gtkrc-2.0
+ mv ~/.bashrc ~/.data/.bashrc; ln -s ~/.data/.bashrc ~/.bashrc
+ mv ~/.bash_history ~/.data/.bash_history; ln -s ~/.data/.bash_history ~/.bash_history
+ mv ~/.bash_profile ~/.data/.bash_profile; ln -s ~/.data/.bash_profile ~/.bash_profile
+ mv ~/.gitconfig ~/.data/.gitconfig; ln -s ~/.data/.gitconfig ~/.gitconfig
